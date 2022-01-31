7223b048dbab467c1c9c8e05d92497a29d7c30d2
Your Name <you@example.com>
2022-01-31 17:41:47 -0300

    boxes challeng local state

--- boxex-challenge-part-um/src/componente/Box.js
+++ boxex-challenge-part-um/src/componente/Box.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
+// export default function Box(props) {
+//   const styles = {
+//       backgroundColor: props.on ? "#222222" : "#cccccc"
+//   }
+
+//   return (
+//     <div style={styles} className="box"></div>
+//   )
+// }
+
+
+// a parti daqui e a aula de boxes challeng parte .1 local state
+
 export default function Box(props) {
+  const [on, setOn] = React.useState(props.on)
+
   const styles = {
-      backgroundColor: props.on ? "#222222" : "#cccccc"
+    backgroundColor: on ? "#222222" : "#cccccc"
+  }
+
+  function toggle(){
+    setOn(prevOn => !prevOn)
   }
 
   return (
-    <div style={styles} className="box"></div>
+    <div style={styles} className="box" onClick={toggle}></div>
   )
 }
-
 
